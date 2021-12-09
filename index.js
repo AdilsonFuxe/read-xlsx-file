906126d47acb8a9fd35288245f7e57925fa71d89
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-12-09 19:02:48 +0100

    refactor: updates

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const xlsx = require('node-xlsx')
+const xlsx = require('node-xlsx');
+//nome do ficheiro excel na pasta do projecto
+const plan = xlsx.parse('Modelos de carros.xlsx');
+const fs = require('fs');
 
-const plan = xlsx.parse('Book1.xlsx');
+const er = /\W/gi;
+const obj = [];
+let i = 0;
+plan[0].data.forEach((item) => {
+  let [Modelo, Classificacao] = item;
 
-let i = 1;
-plan[0].data.map(item => {
-    let dataAtual = 'const data = [\n{\n';
-    let [provincia, codigo, capital, area, populacao, municipios, comunas] = item;
-    
-    dataAtual += `  'id' : ${i++},\n`,
-    dataAtual += `  'nome' : '${provincia}',\n`,  
-    dataAtual += `  'codigo': '${codigo}',\n`,
-    dataAtual += `  'capital': '${capital}',\n`,        
-    dataAtual += `  'area': '${area}',\n`,
-    dataAtual += `  'populacao': '${populacao}',\n`,
-    dataAtual += `  'municipios': ${municipios},\n` ,
-    dataAtual += `  'comunas': ${comunas}\n`,
-    dataAtual += '},\n]\n'
-    console.log(dataAtual);
+  if (item.length > 0 && i > 0) {
+    obj.push({ [Modelo]: Classificacao });
+  }
+  i++;
 });
+
+const save = JSON.stringify(obj);
+fs.writeFileSync('modelos.json', save);
